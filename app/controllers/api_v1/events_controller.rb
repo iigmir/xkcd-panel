63c05b0143404d6b5ba0990a3d9657cde26570f6
iigmir <roc120j@gmail.com>
2019-01-21 15:03:12 +0800

    add http

--- app/controllers/api_v1/events_controller.rb
+++ app/controllers/api_v1/events_controller.rb
@@ -1,2 +1,23 @@
+require "net/http"
+
 class ApiV1::EventsController < ApplicationController
+    def index
+        render :json => { message: "Success" }, :status => 200
+    end
+    def show
+        input_param = params[:id].to_i
+        status = input_param > 0 && input_param != 404 ? 200 : 404
+        res = request_xkcd()
+        render :json => { input_param: input_param, res: res }, status: status
+    end
+
+    private
+    def request_xkcd
+        url = URI.parse('http://www.example.com/index.html')
+        req = Net::HTTP::Get.new(url.to_s)
+        res = Net::HTTP.start(url.host, url.port) {|http|
+            http.request(req)
+        }
+        return res
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "app#index"
-  scope :path => '/api/v1/', :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
-    resources :test
+  scope :path => "/api/v1/", :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
+    get "/test", to: "test#index"
+    get "/event", to: "events#index"
+    get "/event/:id", to: "events#show"
   end
 end
