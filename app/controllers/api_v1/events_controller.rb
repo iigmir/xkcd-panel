92cf596c7a857b5249053d422ad6ad68e3a6e1f2
iigmir <roc120j@gmail.com>
2019-01-21 16:27:17 +0800

    add index

--- app/controllers/api_v1/events_controller.rb
+++ app/controllers/api_v1/events_controller.rb
@@ -2,11 +2,10 @@ require "net/http"
 
 class ApiV1::EventsController < ApplicationController
     def index
-        render :json => { message: "Success" }, :status => 200
+        render :json => { message: "Success", response: request_xkcd("") }, :status => 200
     end
     def show
-        paramenter = params[:id].to_i
-        status = paramenter > 0 ? 200 : 404
+        paramenter = params[:id].to_s + "/"
         response = request_xkcd(paramenter)
         status = response[:code]
         render :json => { paramenter: paramenter, response: response[:body] }, status: status
@@ -14,7 +13,7 @@ class ApiV1::EventsController < ApplicationController
 
     private
     def request_xkcd(input_code)
-        api = "https://xkcd.com/" + input_code.to_s + "/info.0.json"
+        api = "https://xkcd.com/" + input_code + "info.0.json"
         uri = URI(api)
         res = Net::HTTP.get_response(uri)
         json = {}
