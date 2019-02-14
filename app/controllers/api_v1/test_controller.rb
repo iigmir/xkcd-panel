259ee4a564fb733f06c5402396883ac71645ac24
iigmir <roc120j@gmail.com>
2019-02-14 10:18:36 +0800

    Delete test controller

--- app/controllers/api_v1/test_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApiV1::TestController < ApplicationController
-    def index
-        render :json => {
-            message: "Success",
-            version: "1.0",
-            user_agent: request.user_agent
-        }, :status => 200
-    end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "app#index"
   scope :path => "/api/v1/", :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
-    get "/test", to: "test#index"
     get "/xkcd", to: "events#index"
     get "/xkcd/:id", to: "events#show"
   end
